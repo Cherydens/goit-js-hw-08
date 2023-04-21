9c02a9ac2d3f0be7eedc9cd620385b7ed77772dc
Denys Cherednychenko <cherydens@ukr.net>
2023-04-21 16:24:43 +0300

    Complete Task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('ul.gallery');
+const galerryMarkup = createGalerryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galerryMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createGalerryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+        `;
+    })
+    .join('');
+}
