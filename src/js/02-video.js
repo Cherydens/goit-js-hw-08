b6be74592e399b34e48e8d08b338faf7b193b7a1
Denys Cherednychenko <cherydens@ukr.net>
2023-04-21 20:10:14 +0300

    Complete Task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
 
+const _throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTime = localStorage.getItem('videoplayer-current-time')
+  ? JSON.parse(localStorage.getItem('videoplayer-current-time'))
+  : 0;
+
+player.setCurrentTime(currentTime);
+
+player.on('timeupdate', _throttle(getTimeUpdate, 1000));
+
+function getTimeUpdate(data) {
+  console.log(data.seconds);
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
